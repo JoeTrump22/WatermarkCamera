5c2adbeba9c4ddfa5afed9def41f07a4686baf0e
JoeTrump22 <2489491249@qq.com>
2026-07-29 21:22:41 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,3 +2,21 @@ plugins {
     id("com.android.application") version "8.2.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.20" apply false
 }
+
+// ========== 添加阿里云镜像 ==========
+allprojects {
+    repositories {
+        maven {
+            url = uri("https://maven.aliyun.com/repository/public")
+        }
+        maven {
+            url = uri("https://maven.aliyun.com/repository/google")
+        }
+        maven {
+            url = uri("https://maven.aliyun.com/repository/gradle-plugin")
+        }
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
